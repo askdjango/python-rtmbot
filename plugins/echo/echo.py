5c716c2f7788c4464dbb62d01aed7b50b6f2724c
Chinseok Lee <allieuslee@gmail.com>
2015-11-19 01:25:33 +0900

    수정된 메세지에는 대응하지 않음.

--- plugins/echo/echo.py
+++ plugins/echo/echo.py
@@ -3,8 +3,7 @@ outputs = []
 
 
 def process_message(data):
-    if data['channel'].startswith("D"):  # DM channel
-        outputs.append([data['channel'], data['text']])
-    else:
-        outputs.append([data['channel'], '난 DM 에만 메아리친다. :D'])
+    if data['channel'].startswith('D'):  # DM channel
+        if 'text' in data and data['text'].startswith('echo'):
+            outputs.append([data['channel'], data['text']])
 
